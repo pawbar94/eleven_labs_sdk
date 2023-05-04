eebc57c31b55e2cb2204b8d90741191b78f95ef0
pawbar94 <paw.bar94@gmail.com>
2023-05-04 22:28:10 +0200

    CLI skeleton works

--- cli/run_el_cli.py
+++ cli/run_el_cli.py
@@ -2,6 +2,7 @@ import os.path
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
 from cli.utils import read_api_key
+from cli.user_interface.user_interface import UserInterface
 from eleven_labs_api.eleven_labs_api import ElevenLabsApi
 from logging import getLogger
 
@@ -10,6 +11,10 @@ logger = getLogger('ElevenLabsCli')
 if __name__ == "__main__":
     try:
         api: ElevenLabsApi = ElevenLabsApi(api_key=read_api_key())
+        app: UserInterface = UserInterface(sys.argv, api)
+
+        app.init()
+        app.run()
     except Exception as e:
         logger.error(e)
         sys.exit(1)
