b8ed25404b3f24bd0e0cdd7194bc2b8f61f21f25
pawbar94 <paw.bar94@gmail.com>
2023-05-11 22:08:13 +0200

    Fixed params provided for text to speech

--- test/test_api_client/test_input_validation/test_text_to_speech_input_validator.py
+++ test/test_api_client/test_input_validation/test_text_to_speech_input_validator.py
@@ -1,8 +1,14 @@
 import unittest
 
+from api_client.input_validation.exceptions.empty_model_id import EmptyModelId
 from api_client.input_validation.exceptions.empty_text import EmptyText
 from api_client.input_validation.exceptions.empty_voice_id import EmptyVoiceId
+from api_client.input_validation.exceptions.invalid_similarity_boost_value import InvalidSimilarityBoostValue
+from api_client.input_validation.exceptions.invalid_stability_value import InvalidStabilityValue
 from api_client.input_validation.exceptions.missing_latency_argument import MissingLatencyArgument
+from api_client.input_validation.exceptions.missing_model_id_argument import MissingModelIdArgument
+from api_client.input_validation.exceptions.missing_similarity_boost_argument import MissingSimilarityBoostArgument
+from api_client.input_validation.exceptions.missing_stability_argument import MissingStabilityArgument
 from api_client.input_validation.exceptions.missing_text_argument import MissingTextArgument
 from api_client.input_validation.exceptions.missing_voice_id_argument import MissingVoiceIdArgument
 from api_client.input_validation.text_to_speech_input_validator import TextToSpeechInputValidator
@@ -11,35 +17,87 @@ from api_client.input_validation.text_to_speech_input_validator import TextToSpe
 class TestTextToSpeechInputValidator(unittest.TestCase):
     def test_validate_raises_no_exception_for_proper_params(self):
         validator = TextToSpeechInputValidator()
-        validator.validate(voice_id="test ID", text="test text", model_id='some-model-id', stability=0.25,
+        validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=0.25,
                            similarity_boost=0.75, latency=0)
-
-    def test_validate_raise_proper_exception_if_text_not_provided(self):
-        validator = TextToSpeechInputValidator()
-
-        with self.assertRaises(MissingTextArgument):
-            validator.validate(voice_id="test ID", latency=0)
 
     def test_validate_raises_proper_exception_if_voice_id_not_provided(self):
         validator = TextToSpeechInputValidator()
 
         with self.assertRaises(MissingVoiceIdArgument):
-            validator.validate(text="test text", latency=0)
+            validator.validate(text='test text', model_id='some-model-id', stability=0.25, similarity_boost=0.75,
+                               latency=0)
+
+    def test_validate_raise_proper_exception_if_text_not_provided(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(MissingTextArgument):
+            validator.validate(voice_id='test ID', model_id='some-model-id', stability=0.25, similarity_boost=0.75,
+                               latency=0)
+
+    def test_validate_raise_proper_exception_if_model_id_not_provided(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(MissingModelIdArgument):
+            validator.validate(voice_id='test ID', text='test text', stability=0.25, similarity_boost=0.75, latency=0)
+
+    def test_validate_raise_proper_exception_if_stability_id_not_provided(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(MissingStabilityArgument):
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', similarity_boost=0.75,
+                               latency=0)
+
+    def test_validate_raise_proper_exception_if_similarity_boost_id_not_provided(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(MissingSimilarityBoostArgument):
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=0.25,
+                               latency=0)
 
     def test_validate_raises_proper_exception_if_latency_not_provided(self):
         validator = TextToSpeechInputValidator()
 
         with self.assertRaises(MissingLatencyArgument):
-            validator.validate(text="test text", voice_id="test ID")
-
-    def test_validate_raises_proper_exception_if_provided_text_is_empty(self):
-        validator = TextToSpeechInputValidator()
-
-        with self.assertRaises(EmptyText):
-            validator.validate(text="", voice_id="test ID", latency=0)
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=0.25,
+                               similarity_boost=0.75)
 
     def test_validate_raises_proper_exception_if_provided_voice_id_is_empty(self):
         validator = TextToSpeechInputValidator()
 
         with self.assertRaises(EmptyVoiceId):
-            validator.validate(text="test text", voice_id="", latency=0)
+            validator.validate(voice_id='', text='test text', model_id='some-model-id', stability=0.25,
+                               similarity_boost=0.75, latency=0)
+
+    def test_validate_raises_proper_exception_if_provided_text_is_empty(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(EmptyText):
+            validator.validate(voice_id='test ID', text='', model_id='some-model-id', stability=0.25,
+                               similarity_boost=0.75, latency=0)
+
+    def test_validate_raises_proper_exception_if_provided_model_id_is_empty(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(EmptyModelId):
+            validator.validate(voice_id='test ID', text='test text', model_id='', stability=0.25,
+                               similarity_boost=0.75, latency=0)
+
+    def test_validate_raises_proper_exception_if_provided_stability_is_out_of_range(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(InvalidStabilityValue):
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=-20,
+                               similarity_boost=0.75, latency=0)
+        with self.assertRaises(InvalidStabilityValue):
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=20,
+                               similarity_boost=0.75, latency=0)
+
+    def test_validate_raises_proper_exception_if_provided_similarity_boost_is_out_of_range(self):
+        validator = TextToSpeechInputValidator()
+
+        with self.assertRaises(InvalidSimilarityBoostValue):
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=0.25,
+                               similarity_boost=-20, latency=0)
+        with self.assertRaises(InvalidSimilarityBoostValue):
+            validator.validate(voice_id='test ID', text='test text', model_id='some-model-id', stability=0.25,
+                               similarity_boost=20, latency=0)
